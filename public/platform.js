0ea16bf2dd92d918a2429758b3923e8a04b3681d
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2019-09-14 16:44:24 -0600

    Piece creation and matrix

---
+++ public/platform.js
@@ -0,0 +1,43 @@
+class Platform {
+  constructor(
+    rows = 0,
+    cols = 0,
+    x = 0,
+    y = 0,
+    dimension = 25,
+    color = { r: 255, g: 255, b: 255 },
+    margin = 2
+  ) {
+    this.platform = this.generatePlatform(rows, cols);
+    this.dimension = dimension;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.margin = margin;
+  }
+
+  show() {
+    this.platform.forEach((row, i) =>
+      row.forEach((piece, j) =>
+        piece === null ? this.showEmptySpace(i, j) : piece.show()
+      )
+    );
+  }
+
+  generatePlatform(rows, cols) {
+    return Array.from(new Array(rows), row =>
+      Array.from(new Array(cols), col => null)
+    );
+  }
+
+  showEmptySpace(row, col) {
+    let { r, g, b } = this.color;
+    fill(0);
+    stroke(r, g, b);
+    circle(
+      row * this.dimension * this.margin + this.dimension,
+      col * this.dimension * this.margin + this.dimension,
+      this.dimension
+    );
+  }
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,39 @@
+const diameter = 25;
+const xwidth = 600;
+const yheight = 600;
+
+let piece = new Piece(xwidth / 2, yheight / 2, diameter, {
+  r: 250,
+  g: 10,
+  b: 0
+});
+
+let platform = new Platform(
+  7,
+  7,
+  0,
+  0,
+  50,
+  {
+    r: 255,
+    g: 255,
+    b: 255
+  },
+  1.7
+);
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(xwidth, yheight);
 }
 
 function draw() {
   background(0);
+  platform.show();
+  piece.show();
+  movementOfPiece(piece);
 }
+
+let movementOfPiece = p => {
+  p.x = mouseX;
+  p.y = mouseY;
+};
