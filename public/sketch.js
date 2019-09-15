ee9d6d2ca880b43450640d24448d0433421a4e1e
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2019-09-15 15:53:59 -0600

    Chapter 3 - Placement of pieces

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,13 @@
-const diameter = 25
+const diameter = 50
 const xwidth = 600
 const yheight = 600
+const rows = 7
+const cols = 7
+const platforMargin = 1.7
 
 let piece = new Piece(xwidth / 2, yheight / 2, diameter, {r: 250, g: 10, b: 0})
 
-let platform = new Platform(7, 7, 0, 0, 50,{r: 0, g: 0, b: 0}, 1.7)
+let platform = new Platform(rows, cols, 0, 0, diameter, { r: 0, g: 0, b: 0 }, platforMargin)
 
 function setup() {
   let canv = createCanvas(xwidth, yheight)
@@ -21,4 +24,14 @@ function draw() {
 let movementOfPiece = p => {
   p.x = mouseX
   p.y = mouseY
+}
+
+let putPieceOnPlatfrom = (piece, platform) => {
+  let mapX = map(piece.x, 0, width, 0, rows)
+  let mapY = map(piece.y, 0, height, 0, cols)
+  platform.platform[floor(mapX)][floor(mapY)] = new Piece(piece.x, piece.y, piece.diameter, piece.color)
+}
+
+function mouseClicked() {
+  putPieceOnPlatfrom(piece, platform)
 }
